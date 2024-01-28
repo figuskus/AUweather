eb386ecdd132434052311f5ef0a91a3ca3678da0
figuskus <146824421+figuskus@users.noreply.github.com>
2024-01-28 05:14:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ def main():
         data = np.array([int(Week),Location[miasto],weather[0],weather[1],weather[2],weather[3],weather[4],weather[5],weather[6],weather[7],weather[8],weather[9],weather[10]]).reshape(1, -1)
         rain = model.predict(data)
         s_confidence = model.predict_proba(data)
-        pewnosc = s_confidence[0]*100)
+        pewnosc = s_confidence[0]*100
         # st.write(s_confidence)
         st.header("Czy będzie jutro padać w Australii?{0}".format("Tak" if rain[0] == 1 else "Nie"))
         # st.subheader("Pewność predykcji {0:.2f} %".format(s_confidence[0][rain][0]*100))
