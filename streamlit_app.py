56cc7bf8708afc195a33ce851ef6567f74cb3834
figuskus <146824421+figuskus@users.noreply.github.com>
2024-01-28 04:55:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,9 @@ def main():
         st.write(rain)
         print(rain)
         st.header("Czy będzie jutro padać w Australii?{0}".format("Tak" if rain[0] == 1 else "Nie"))
+        # st.subheader("Pewność predykcji {0:.2f} %".format(s_confidence[0][rain][0]*100))
         st.subheader("Pewność predykcji {0:.2f} %".format(s_confidence[0][rain][0]*100))
+
 
 if __name__  == "__main__":
     main()
